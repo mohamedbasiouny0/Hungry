f28aae818703303cee521e945a8cfd5b5eeb988e
Mohamed <mido.basiouny123@gmail.com>
2026-04-20 01:16:51 +0200

    feat: add AppStyles

---
+++ lib/core/constant/app_styles.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:test2/core/constant/app_colors.dart';
+
+class AppStyles {
+  static final TextStyle popMedium18 = GoogleFonts.poppins(
+    fontWeight: .w500,
+    fontSize: 18,
+    color: AppColors.hardGreyColor,
+  );
+  static final TextStyle popSemiBold20 = GoogleFonts.poppins(
+    fontWeight: .w600,
+    fontSize: 20,
+    color: AppColors.primaryFontColor,
+  );
+  static final TextStyle popBold30 = GoogleFonts.poppins(
+    fontWeight: .w700,
+    fontSize: 30,
+    color: AppColors.primaryColor,
+  );
+  static final TextStyle interMedium16 = GoogleFonts.inter(
+    fontWeight: .w500,
+    fontSize: 16,
+    color: AppColors.hardGreyColor,
+  );
+  static final TextStyle interSemiBold18 = GoogleFonts.inter(
+    fontWeight: .w600,
+    fontSize: 18,
+    color: Colors.white,
+  );
+  static final TextStyle robtoMedium18 = GoogleFonts.roboto(
+    fontWeight: .w500,
+    fontSize: 18,
+    color: AppColors.primaryFontColor,
+  );
+  static final TextStyle robtoMedium20 = GoogleFonts.roboto(
+    fontWeight: .w500,
+    fontSize: 20,
+    color: Colors.white,
+  );
+  static final TextStyle robtoSemiBold16 = GoogleFonts.roboto(
+    fontWeight: .w600,
+    fontSize: 16,
+    color: AppColors.primaryFontColor,
+  );
+  static final TextStyle robtoRegular16 = GoogleFonts.roboto(
+    fontWeight: .w400,
+    fontSize: 16,
+    color: AppColors.primaryFontColor,
+  );
+  static final TextStyle robtoRegular14 = GoogleFonts.roboto(
+    fontWeight: .w400,
+    fontSize: 14,
+    color: AppColors.primaryFontColor,
+  );
+  static final TextStyle robtoMedium16 = GoogleFonts.roboto(
+    fontWeight: .w500,
+    fontSize: 16,
+    color: AppColors.primaryFontColor,
+  );
+  static final TextStyle robtoExtraBold16 = GoogleFonts.roboto(
+    fontWeight: .w800,
+    fontSize: 16,
+    color: AppColors.primaryFontColor,
+  );
+  static final TextStyle robtoRegular18 = GoogleFonts.roboto(
+    fontWeight: .w400,
+    fontSize: 18,
+    color: AppColors.mediumGreyColor,
+  );
+  static final TextStyle robtoMeduim14 = GoogleFonts.roboto(
+    fontWeight: .w500,
+    fontSize: 14,
+    color: AppColors.primaryFontColor,
+  );
+  static final TextStyle robtoSemiBold18 = GoogleFonts.roboto(
+    fontWeight: .w600,
+    fontSize: 18,
+    color: AppColors.primaryFontColor,
+  );
+  static final TextStyle robtoSemiBold14 = GoogleFonts.roboto(
+    fontWeight: .w600,
+    fontSize: 14,
+    color: AppColors.primaryFontColor,
+  );
+  static final TextStyle robtoMeduim12 = GoogleFonts.roboto(
+    fontWeight: .w500,
+    fontSize: 12,
+    color: Colors.white,
+  );
+  static final TextStyle reemKufiRegular32 = GoogleFonts.reemKufiInk(
+    fontWeight: .w400,
+    fontSize: 32,
+    color: AppColors.primaryFontColor,
+  );
+  static final TextStyle reemKufiRegular18 = GoogleFonts.reemKufiInk(
+    fontWeight: .w400,
+    fontSize: 18,
+    color: AppColors.mediumGreyColor,
+  );
+  static final TextStyle reemKufiBold18 = GoogleFonts.reemKufiInk(
+    fontWeight: .w700,
+    fontSize: 18,
+    color: AppColors.mediumGreyColor,
+  );
+}
