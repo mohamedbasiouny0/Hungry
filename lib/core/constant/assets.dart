ab061901cca66617a8611f21470ed2622e6044c4
Mohamed <mido.basiouny123@gmail.com>
2026-04-20 01:43:03 +0200

    feat: add Assets file to the project

---
+++ lib/core/constant/assets.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  static const String imagesHungryLogo = "assets/images/Hungry_logo.svg";
+
+  static const String imagesTestImage = "assets/images/test_image.svg";
+}
+
