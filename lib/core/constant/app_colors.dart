3f9becec05e58a11ba1018692cc7022d28af2017
Mohamed <mido.basiouny123@gmail.com>
2026-04-20 01:06:33 +0200

    feat: add new colors to AppColors

--- lib/core/constant/app_colors.dart
+++ lib/core/constant/app_colors.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color primaryColor = Color(0xff08431D);
   static const Color primaryFontColor = Color(0xff3C2F2F);
-  static const Color softGreyColor = Color(0xff7D7D7D);
-  static const Color mediumGreyColor = Color(0xff6A6A6A);
-  static const Color hardGreyColor = Color(0xff808080);
+  static const Color mediumGreyColor = Color(0xff7D7D7D);
+  static const Color hardGreyColor = Color(0xff6A6A6A);
+  static const Color softGreyColor = Color(0xff808080);
 }
