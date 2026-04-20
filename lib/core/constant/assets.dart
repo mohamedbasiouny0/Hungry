e39548790b06241f493fcf5134efe0fd9f9fedb4
Mohamed <mido.basiouny123@gmail.com>
2026-04-20 02:19:15 +0200

    feat: create splashView for Hungry

--- lib/core/constant/assets.dart
+++ lib/core/constant/assets.dart
@@ -1,9 +1,8 @@
 // ignore_for_file: prefer_single_quotes
 class Assets {
   Assets._();
-  
+
   static const String imagesHungryLogo = "assets/images/Hungry_logo.svg";
 
-  static const String imagesTestImage = "assets/images/test_image.svg";
+  static const String imagesTestImage = "assets/images/test_image.png";
 }
-
