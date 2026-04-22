4707517f2e3340679cdeb2514279837258b58cab
Mohamed <mido.basiouny123@gmail.com>
2026-04-22 17:55:02 +0200

    feat: create HomeAppBar (Header)

--- lib/core/constant/assets.dart
+++ lib/core/constant/assets.dart
@@ -1,8 +1,11 @@
 // ignore_for_file: prefer_single_quotes
 class Assets {
   Assets._();
+  
+  static const String imagesAvatar = "assets/images/avatar.png";
 
   static const String imagesHungryLogo = "assets/images/Hungry_logo.svg";
 
   static const String imagesTestImage = "assets/images/test_image.png";
 }
+
--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:test2/features/home/presentation/views/widgets/home_app_bar.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('This is HomeView')));
+    return const Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          HomeAppBar(),
+          
+        ],
+      ),
+    );
   }
 }
