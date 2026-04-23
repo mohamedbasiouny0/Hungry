b9fca49d282866cb6789af46e6e93366b010b6af
Mohamed <mido.basiouny123@gmail.com>
2026-04-23 19:21:43 +0200

    merge: Merge root into main

---
+++ lib/features/home/presentation/views/widgets/user_header.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:test2/core/constant/app_colors.dart';
+import 'package:test2/core/constant/app_styles.dart';
+import 'package:test2/core/constant/assets.dart';
+
+class UserHeader extends StatelessWidget {
+  const UserHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: .start,
+      mainAxisAlignment: .spaceBetween,
+      children: [
+        Column(
+          crossAxisAlignment: .start,
+          children: [
+            SvgPicture.asset(
+              Assets.imagesHungryLogo,
+              width: 240,
+              colorFilter: const ColorFilter.mode(
+                AppColors.primaryColor,
+                .srcIn,
+              ),
+            ),
+            Text('Hello, Mohamed Basiouny', style: AppStyles.popMedium18),
+          ],
+        ),
+        ClipRRect(
+          borderRadius: .circular(20),
+          child: Image.asset(Assets.imagesAvatar, height: 60, width: 60),
+        ),
+      ],
+    );
+  }
+}
