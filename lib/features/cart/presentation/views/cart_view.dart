17143f611165aa3cacdc98b3da53d15dc1778577
Mohamed <mido.basiouny123@gmail.com>
2026-04-26 14:02:01 +0300

    feat: enhance CartView with custom AppBar and layout adjustments

--- lib/features/cart/presentation/views/cart_view.dart
+++ lib/features/cart/presentation/views/cart_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:test2/features/cart/presentation/views/widgets/cart_item.dart';
+import 'package:test2/features/product/presentation/views/widgets/custom_buttom_row.dart';
 
 class CartView extends StatefulWidget {
   const CartView({super.key});
@@ -20,26 +21,43 @@ class _CartViewState extends State<CartView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: ListView.builder(
-        itemCount: 5,
-        itemBuilder: (BuildContext context, int index) {
-          return CartItem(
-            itemNumber: itemsNumber[index],
-            plusOnPressed: () {
-              setState(() {
-                itemsNumber[index]++;
-              });
-            },
-            minusOnPressed: () {
-              setState(() {
-                if (itemsNumber[index] > 0) {
-                  itemsNumber[index]--;
-                }
-              });
-            },
-          );
-        },
+      appBar: AppBar(
+        toolbarHeight: 100,
+        title: Container(
+          padding: const .symmetric(horizontal: 8),
+          child: CustomButtomRow(text: 'Checkout', onPressed: () {}),
+        ),
+        elevation: 0,
+        scrolledUnderElevation: 4,
+        surfaceTintColor: Colors.transparent,
+        shadowColor: Colors.black,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              padding: const .only(bottom: 93),
+              itemCount: 5,
+              itemBuilder: (BuildContext context, int index) {
+                return CartItem(
+                  itemNumber: itemsNumber[index],
+                  plusOnPressed: () {
+                    setState(() {
+                      itemsNumber[index]++;
+                    });
+                  },
+                  minusOnPressed: () {
+                    setState(() {
+                      if (itemsNumber[index] > 0) {
+                        itemsNumber[index]--;
+                      }
+                    });
+                  },
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
