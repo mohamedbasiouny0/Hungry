9e64d91b75abe43e931c4f7a1814974d6f116843
Mohamed <mido.basiouny123@gmail.com>
2026-04-27 16:40:44 +0300

    feat: Create OrderHistoryItem

--- lib/features/order_histroy/presentation/views/order_history_view.dart
+++ lib/features/order_histroy/presentation/views/order_history_view.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:test2/features/order_histroy/presentation/views/widgets/order_history_item.dart';
 
 class OrderHistoryView extends StatelessWidget {
   const OrderHistoryView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('This is Order history')));
+    return const Scaffold(body: OrderHistoryItem());
   }
 }
