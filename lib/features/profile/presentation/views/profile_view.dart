14c448fae8cf39ecd0b5300e0db04cc8a68dd3a1
Mohamed <mido.basiouny123@gmail.com>
2026-04-27 15:23:00 +0300

    feat: Create ProfilePicture

--- lib/features/profile/presentation/views/profile_view.dart
+++ lib/features/profile/presentation/views/profile_view.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gap/gap.dart';
+import 'package:test2/core/constant/app_colors.dart';
+import 'package:test2/core/constant/assets.dart';
+import 'package:test2/features/profile/presentation/views/widgets/profile_picture.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('This is Profile')));
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            Gap(78.h),
+            const ProfilePicture(image: Assets.imagesAvatar),
+          ],
+        ),
+      ),
+    );
   }
 }
