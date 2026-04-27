cf84ff5cb6b1d9c736033284178b2c9ba59163af
Mohamed <mido.basiouny123@gmail.com>
2026-04-27 15:36:45 +0300

    feat: Update ProfileView and add ProfileListTile widget

--- lib/features/profile/presentation/views/profile_view.dart
+++ lib/features/profile/presentation/views/profile_view.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:gap/gap.dart';
+import 'package:test2/core/constant/app_colors.dart';
+import 'package:test2/core/constant/app_styles.dart';
 import 'package:test2/core/constant/assets.dart';
+import 'package:test2/features/checkout/presentation/views/widgets/payment_options.dart';
+import 'package:test2/features/profile/presentation/views/widgets/profile_list_tile.dart';
 import 'package:test2/features/profile/presentation/views/widgets/profile_picture.dart';
 import 'package:test2/features/profile/presentation/views/widgets/profile_text_field.dart';
 
@@ -24,7 +29,14 @@ class ProfileView extends StatelessWidget {
             Gap(25.h),
             const ProfileTextField(labelText: 'Delivery Address'),
             Gap(25.h),
-            const ProfileTextField(labelText: 'Name'),
+            const ProfileTextField(labelText: 'Password'),
+            Divider(
+              height: 72,
+              color: AppColors.mediumGreyColor,
+              endIndent: 25.w,
+              indent: 25.w,
+            ),
+            const ProfileListTile(),
           ],
         ),
       ),
