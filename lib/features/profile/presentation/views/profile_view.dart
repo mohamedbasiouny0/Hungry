7a2aa3967e38194315142120e36bce71a3d8a17e
Mohamed <mido.basiouny123@gmail.com>
2026-04-27 15:24:55 +0300

    feat: Create ProfileTextField

--- lib/features/profile/presentation/views/profile_view.dart
+++ lib/features/profile/presentation/views/profile_view.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gap/gap.dart';
-import 'package:test2/core/constant/app_colors.dart';
 import 'package:test2/core/constant/assets.dart';
 import 'package:test2/features/profile/presentation/views/widgets/profile_picture.dart';
+import 'package:test2/features/profile/presentation/views/widgets/profile_text_field.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
@@ -17,6 +17,14 @@ class ProfileView extends StatelessWidget {
           children: [
             Gap(78.h),
             const ProfilePicture(image: Assets.imagesAvatar),
+            Gap(35.h),
+            const ProfileTextField(labelText: 'Name'),
+            Gap(25.h),
+            const ProfileTextField(labelText: 'Email'),
+            Gap(25.h),
+            const ProfileTextField(labelText: 'Delivery Address'),
+            Gap(25.h),
+            const ProfileTextField(labelText: 'Name'),
           ],
         ),
       ),
